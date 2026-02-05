a9425bc048d8b44135228854ff951385db9a4b61
welsondeivid <53789831+welsondeivid@users.noreply.github.com>
2026-02-05 13:16:57 -0300

    :construction: feat: Roteamento

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home.jsx";
 function App() {
   return (
-    <div>
-      <h1>Sistema de Estoque</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+function Home(params) {
+    return (
+        <div>Home Page</div>
+    );
+}
+
+export default Home;
