158144ef7bf8863d43fa34c2e9dbcc2b593f9e11
welsondeivid <53789831+welsondeivid@users.noreply.github.com>
2026-02-06 21:17:12 -0300

    :sparkles: feat:Listagem dos produtods produzíveis ordenados

--- src/pages/Production.jsx
+++ src/pages/Production.jsx
@@ -2,9 +2,15 @@ import { useState, useEffect } from "react";
 
 import { availables } from "../api/productService";
 
+import Toast from "../components/Toast";
+
+import "../styles/tabs.css";
+
+import { mapProductError } from "../utils/Error";
+
 function AvailableProductsList() {
   const [availableProducts, setAvailableProducts] = useState([]);
-  const [error, setError] = useState(null);
+  const [toast, setToast] = useState(null);
 
     useEffect(() => {
         async function getAvailableProducts() {
@@ -12,7 +18,7 @@ function AvailableProductsList() {
                 const data = await availables();
                 setAvailableProducts(data);
             } catch (error) {
-                setError(error.message);
+                setToast({ message: mapProductError(error), type: "error" });
                 console.error("Error fetching available products:", error);
             }
         }
@@ -21,19 +27,43 @@ function AvailableProductsList() {
     }, []);
 
     return (
-        <div>
-            {error && <p style={{ color: 'red' }}>Erro: {error}</p>}
-            <h2>Produtos Disponíveis para Produção</h2>
-            
-            <ul>
-            {availableProducts.map(element => (
-                <li key={element.productCode}> 
-                <strong>{element.productCode}</strong> - {element.productName} - Quantidade disponível: {element.maxProduction}
-                - valor Unitário: R$ {element.unitValue} - valor Total: R$ {element.totalValue}
-                </li>
-            ))}
-            </ul>
-        </div>
+        <>
+            <div className="main">
+                
+                <h1>Produtos Disponíveis</h1>
+                
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Código</th>
+                            <th>Nome</th>
+                            <th>Máximo possível</th>
+                            <th>Valor unitário</th>
+                            <th>Valor total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {availableProducts.map(element => (
+                            <tr key={element.productCode}>
+                                <td>{element.productCode}</td>
+                                <td>{element.productName}</td>
+                                <td>{element.maxProduction} unidades</td>
+                                <td>R$ {element.unitValue}</td>
+                                <td>R$ {element.totalValue}</td>
+                            </tr>))
+                        }
+                    </tbody>
+                </table>
+            </div>
+
+            {toast && (
+                <Toast
+                    type={toast.type}
+                    message={toast.message}
+                    onClose={() => setToast(null)}
+                />
+            )}
+        </>
     )
 }
 
